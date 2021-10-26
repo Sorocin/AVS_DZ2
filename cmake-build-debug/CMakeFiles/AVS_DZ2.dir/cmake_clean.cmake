4051e0f42c2bbf40d8f17c837a78ad2551ed0fea
Александр <soroca027473@outlook.com>
2021-10-26 22:27:40 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/AVS_DZ2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "AVS_DZ2.exe"
+  "AVS_DZ2.pdb"
+  "CMakeFiles/AVS_DZ2.dir/main.cpp.o"
+  "libAVS_DZ2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AVS_DZ2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/AVS_DZ2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for AVS_DZ2.
+# This may be replaced when dependencies are built.
